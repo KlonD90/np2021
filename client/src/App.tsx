9f6fb31ff6a807ae00b59ac621a23c7960b3f4bd
KuBerlinON <bkuberlinov@gmail.com>
2021-09-02 15:46:15 +0200

    footer component added, landing page lements added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/main.module.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import LandingPage from './components/LandingPage';
--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -1,13 +1,42 @@
 import React from 'react';
-import { Typography, Container } from '@material-ui/core'
+import { Typography, Container, Grid } from '@material-ui/core';
+import '../styles/landingpg.css'
 
 const LandingPage = () => {
 
     return (
-        <Container>
+        <Container maxWidth="md">
             <main className="landing-pg">
-                <Typography variant="h3">Наблюдательный Полк 2021 (Калмыкия)</Typography>
-
+                <Typography
+                    variant="h6"
+                    component="h1"
+                    gutterBottom
+                    align="center"
+                >
+                    Наблюдательный Полк 2021 (Калмыкия
+                </Typography>
+                <Grid container
+                    spacing={2}
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Grid item
+                        xs={12}
+                    >
+                        информационная панель
+                    </Grid>
+                    <Grid item
+                        xs={12}
+                    >
+                        карта калмыкии
+                    </Grid>
+                    <Grid item
+                        xs={12}
+                    >
+                        сводная таблица по тикам
+                    </Grid>
+                </Grid>
             </main>
         </Container>
     )
